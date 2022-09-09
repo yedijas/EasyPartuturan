4d9703694405f32ac20170ecfee894a3977bcd40
yedijas <bananab9001@gmail.com>
2022-09-09 10:47:46 +0700

    Add project files.

---
+++ easypartuturan.logic/Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easypartuturan.logic
+{
+    public enum BranchDirection { 
+        TENGAH,
+        ATAS,
+        KANAN,
+        BAWAH,
+        KIRI
+    }
+
+    public class Tree
+    {
+        public string Name { get; set; }
+        public List<Tree> Branch { get; set; }
+        public BranchDirection Direction { get; set; }
+    }
+}
