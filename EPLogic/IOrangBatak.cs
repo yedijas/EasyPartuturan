7b3c8dec97656f04d03a187505a7185656bd6139
yedijas <bananab9001@gmail.com>
2024-11-17 00:37:12 +0700

    recreate using tree

---
+++ EPLogic/IOrangBatak.cs
@@ -0,0 +1,14 @@
+﻿namespace EPLogic
+{
+    public interface IOrangBatak
+    {
+        public bool MamaknyaSi(IOrangBatak kawanIni);
+        public bool BapaknyaSi(IOrangBatak kawanIni);
+        public bool AnaknyaSi(IOrangBatak kawanIni);
+        public bool BorunyaSi(IOrangBatak kawanIni);
+        public bool KawinSama(IOrangBatak kawanIni);
+        public bool NamanyaSi(IOrangBatak kawanIni, string nama);
+        public bool MarganyaSi(IOrangBatak kawanIni, string marga);
+        public string Dipanggil();
+    }
+}
---
+++ EPLogic/OrangBatak.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPLogic
+{
+    public class OrangBatak
+    {
+        #region variables
+        private string nama;
+        private string marga;
+        private Guid id;
+        private OrangBatak bapak;
+        private OrangBatak mamak;
+        private OrangBatak pasangan;
+        private OrangBatak abang;
+        private OrangBatak kakak;
+        private OrangBatak anak;
+        private OrangBatak boru;
+        private string panggilan;
+        #endregion
+
+        #region getter setter
+        public string Nama { get { return nama; } set { nama = value; } }
+        public string Marga { get { return marga; } set { marga = value; } }
+        public Guid Id { get { return id; } set { id = value; } }
+        public OrangBatak Bapak { get { return bapak; } set { bapak = value; } }
+        public OrangBatak Mamak { get { return mamak; } set { mamak = value; } }
+        public OrangBatak Pasangan { get { return pasangan; } set { pasangan = value; } }
+        public OrangBatak Abang { get { return abang; } set { abang = value; } }
+        public OrangBatak Kakak { get { return kakak; } set { kakak = value; } }
+        public OrangBatak Anak { get { return anak; } set { anak = value; } }
+        public OrangBatak Boru { get { return boru; } set { boru = value; } }
+        public string Panggilan { get { return panggilan; } set { panggilan = value; } }
+        #endregion
+
+        #region constructor
+        public OrangBatak()
+        {
+            nama = "";
+            marga = "";
+            id = Guid.NewGuid();
+            mamak = null; bapak = null; pasangan = null; abang = null; kakak = null; anak = null; boru = null;
+        }
+
+        public OrangBatak(string _panggilan)
+        {
+            nama = "";
+            marga = "";
+            id = Guid.NewGuid();
+            panggilan = _panggilan;
+            mamak = null; bapak = null; pasangan = null; abang = null; kakak = null; anak = null; boru = null;
+        }
+        #endregion
+
+        #region IOrangBatak impl
+        public bool AnaknyaSi(OrangBatak kawanIni)
+        {
+            try
+            {
+                if (kawanIni == null ||
+                    kawanIni.Equals(this))
+                {
+                    throw new InvalidOperationException("yakalik bapaknya hampa!");
+                }
+                if (kawanIni.Boru.Equals(this))
+                {
+                    throw new InvalidOperationException("jadi kau cw atau cw?! mau jadi anak sama boru bah!");
+                }
+                this.bapak = kawanIni;
+                if (kawanIni.Anak == null ||
+                    !kawanIni.Anak.Equals(this))
+                {
+                    kawanIni.BapaknyaSi(this);
+                }
+                else
+                {
+                    throw new InvalidOperationException("anak yatim lu!");
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+
+        public bool BorunyaSi(OrangBatak kawanIni)
+        {
+            try
+            {
+                if (kawanIni == null ||
+                    kawanIni.Equals(this))
+                {
+                    throw new InvalidOperationException("yakalik bapaknya hampa!");
+                }
+                if (kawanIni.Anak.Equals(this))
+                {
+                    throw new InvalidOperationException("jadi kau cw atau cw?! mau jadi anak sama boru bah!");
+                }
+                this.bapak = kawanIni;
+                if (kawanIni.Boru == null ||
+                    !kawanIni.Boru.Equals(this))
+                {
+                    kawanIni.BapaknyaSi(this);
+                }
+                else
+                {
+                    throw new InvalidOperationException("anak yatim lu!");
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+
+        public bool BapaknyaSi(OrangBatak kawanIni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool KawinSama(OrangBatak kawanIni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MamaknyaSi(OrangBatak kawanIni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MarganyaSi(OrangBatak kawanIni, string marga)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NamanyaSi(OrangBatak kawanIni, string nama)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region overrides
+
+        #endregion
+    }
+}
